09c34b28f54af3572847bf2f8d590a82d00d5515
amaresh329 <vutukuriamaresh930@gmail.com>
2025-07-10 12:52:59 +0530

    Initiali Commit

--- app/layout.js
+++ app/layout.js
@@ -22,7 +22,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+       <main>
         {children}
+       </main>
+        
       </body>
     </html>
   );
