ae8e1a1e1e0e070bc15a9c88b1942185fea1ed21
amaresh329 <vutukuriamaresh930@gmail.com>
2025-07-14 21:23:16 +0530

    added header component and images in the public

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,22 +1,28 @@
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
-const Header = () => {
+const Header = async({isAdminPage=false}) => {
+  const isAdmin=false;
   return (
-    <div>
-          {/* <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn> */}
-            Header
-    </div>
+    <header className='fixed top-0 w-full bg-white-80 backdrop-blur-md z-50 border-b'>
+      <nav className='mx-auto px-4 py-4 flex items-center justify-between'>
+        <Link href={isAdminPage?"/admin":"/"}>
+          <Image
+            src={'/logo.png'}
+            alt="vehiql logo"
+            width={200}
+            height={60}
+            className='h-12 w-auto object-contain'
+          />
+          {
+            isAdminPage && (
+              <span className='text-xs font-extralight'>admin</span>
+            )
+          }
+        </Link>
+      </nav>
+    </header>
   )
 }
 
